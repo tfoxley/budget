8e14d3d15c1c1c56c81df15ed763973b6901d25a
Travis Foxley <travisfoxley@gmail.com>
2011-06-23 09:50:06 -0600

    same

--- db/migrate/20110623152429_add_default_value_for_completed.rb
+++ db/migrate/20110623152429_add_default_value_for_completed.rb
@@ -1,7 +1,7 @@
 class AddDefaultValueForCompleted < ActiveRecord::Migration
   def self.up
     remove_column :list_items, :completed
-    add_column :list_items, :completed, :boolean, :default => false
+    add_column :list_items, :completed, :boolean, :default => 0
   end
 
   def self.down
