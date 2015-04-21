126005ea13d138ac9f541d3aa7d4dbc19e83700f
Travis Foxley <travis@travis-mbp.local>
2015-04-20 22:24:32 -0400

    Update a few gem versions and get app running

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.19'
+gem 'rails', '3.0.20'
 #gem 'mobylette', '2.0'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3', '1.3.6'
+gem 'sqlite3', '1.3.10'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Toolbox
   class Application < Rails::Application
+    ActionController::Base.config.relative_url_root = ''
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
